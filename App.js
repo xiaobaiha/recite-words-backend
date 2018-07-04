e852fbc169798af082620840f67f15fb4454e079
CN\feizhijun <xiaobaihaha0001@163.com>
2018-07-04 17:21:51 +0800

    @dev add rtx logger

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// logger
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// response
 app.use(async ctx => {
     console.log(ctx.request.url);
     ctx.body = "Hello world!"
