845b71e707821ce303dee04a03ba31701633d370
feizhijun <feizhijun@bytedance.com>
2019-01-07 10:14:44 +0800

    feat: login

--- src/app/controller/login.js
+++ src/app/controller/login.js
@@ -1,7 +1,16 @@
 const parse = require('co-body');
+const {login} = require('../service/db');
 
 module.exports = async ctx => {
     const {name, password} = await parse(ctx.request);
-    console.log({name, password});
-    ctx.body = "Hello world!"
+    const flag = await login(name, password);
+    if (flag) {
+        ctx.status = 200;
+        ctx.message = "Login success";
+        ctx.body = {result: true};
+    } else {
+        ctx.status = 601;
+        ctx.message = "Login failed";
+        ctx.body = {result: false};
+    }
 }
--- src/app/service/db.js
+++ src/app/service/db.js
@@ -7,7 +7,18 @@ connection.connect();
 
 const login = (name, pwd) => {
     return new Promise((resolve, reject) => {
-        connection.query("SELECT ")
-    });
+        connection.query(`SELECT password FROM user WHERE name="${name}"`, (err, results, fields) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results[0].password === pwd)
+            }
+        })
+    }).catch(err => {
+        console.error(err);
+    }) ;
 }
 
+module.exports = {
+    login
+}
